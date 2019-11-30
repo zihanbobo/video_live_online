09aacd15930f70ad6d41a499a2d178d9418f70dd
dyh1103 <1183111379@qq.com>
2019-11-30 15:49:07 +0800

    提交druid配置

--- src/main/java/com/video/live/VideoLiveApplication.java
+++ src/main/java/com/video/live/VideoLiveApplication.java
@@ -2,8 +2,10 @@ package com.video.live;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.video.live.entity")
 public class VideoLiveApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/video/live/VideoLiveApplicationTests.java
+++ src/test/java/com/video/live/VideoLiveApplicationTests.java
@@ -1,6 +1,6 @@
 package com.video.live;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
