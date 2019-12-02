db911c518518a74d1dcb9668b90e0ceb585b0bdc
dyh1183 <1183111379@qq.com>
2019-12-02 16:28:12 +0800

    修改日志配置

--- src/main/java/com/video/live/VideoLiveApplication.java
+++ src/main/java/com/video/live/VideoLiveApplication.java
@@ -1,13 +1,14 @@
 package com.video.live;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.video.live.entity")
 public class VideoLiveApplication {
-
+    private static final Logger logger = LoggerFactory.getLogger(VideoLiveApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(VideoLiveApplication.class, args);
     }
